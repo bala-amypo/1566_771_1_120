657d1935c7ae1ac15e0c78b1c6175ca4b4c77de0
bala-amypo <bala@amypo.in>
2025-12-19 10:12:21 +0000

    User commit

--- src/main/java/com/example/demo/service/ApiUsageLogService.java
+++ src/main/java/com/example/demo/service/ApiUsageLogService.java
@@ -1,9 +1,5 @@
 package com.example.demo.service;
 
-import com.example.demo.entity.ApiUsageLog;
-import java.util.List;
-
 public interface ApiUsageLogService {
-    void log(String apiKey);
-    List<ApiUsageLog> getLogs(String apiKey);
+    void logUsage(String apiKey);
 }
